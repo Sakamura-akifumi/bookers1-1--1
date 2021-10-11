9775c8058f2a517364ba928a84079e18b05bcfee
Sakamura-akifumi <tmr20th08moon@gmail.com>
2021-10-11 07:59:10 +0000

    finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was succesfully created'
+    else
+      render :book_path
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was succesfully update"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def dstroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to :books_path
+  end
+
+  private
+  def book_params
+   params.require(:book).permit(:title, :body)
+  end
+end
